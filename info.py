b5162499ceece611d25b1cd88ddad397999f0278
spidyynaik <128451864+spidyynaik@users.noreply.github.com>
2023-06-01 17:04:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,3 +93,9 @@ LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_I
 LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
+
+#Verify Features
+VERIFY = bool(environ.get('VERIFY', True))
+HOW_TO_VERIFY = environ.get("HOW_TO_VERIFY", "https://t.me/YourDemandZone/105")
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tinyfy.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '7c31dde847fd4e12d0cba821e8e36149e3935760')
